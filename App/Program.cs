42a2239ed748da095a5e2e099ab047b96f7839a9
KyroVibe <hunter.barclay18@gmail.com>
2021-12-03 18:48:48 -0800

    Project setup

---
+++ App/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Maskophf_App {
+    class Program {
+        static void Main(string[] args) {
+            Maskophf_Lib.PNGLoader.Test();
+        }
+    }
+}
---
+++ Lib/PNGLoader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Maskophf_Lib {
+    public class PNG {
+
+        private byte[] _raw;
+
+        public Color[][] Pixels;
+
+        public PNG(string path)
+            => LoadFrom(File.ReadAllBytes(path));
+
+        public PNG(byte[] data)
+            => LoadFrom(data);
+
+        private void LoadFrom(byte[] data) {
+            _raw = data;
+        }
+
+        public void WriteTo(string path)
+            => WriteTo(File.Create(path));
+
+        public void WriteTo(Stream s) {
+            var sw = new StreamWriter(s);
+            // TODO
+            sw.Flush();
+            sw.Close();
+        }
+    }
+
+    public struct Header {
+        
+    }
+
+    public struct Color {
+        public int R, G, B, A;
+    }
+}
